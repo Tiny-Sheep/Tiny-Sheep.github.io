7e36db33ebf2b78c4bec8c746c604e6043050487
Lamine Sadoun <laminesadoun@Lamines-MacBook-Pro.local>
2023-03-26 13:26:59 -0400

    second iteration

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("cards").onmousemove = e => {
+    console.log('is this working?')
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
